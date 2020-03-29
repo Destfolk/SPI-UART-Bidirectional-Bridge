9187001ef7cbce831998e1665e944b82b445b51c
Destfolk <36056285+Destfolk@users.noreply.github.com>
2020-03-29 12:13:57 +0200

    Update tb.vhd

--- tb.vhd
+++ tb.vhd
@@ -8,6 +8,7 @@
 --Email: destfolk@gmail.com
 ----------------------------------------------------------------------------
 
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.all;
@@ -18,25 +19,39 @@ end tb;
 architecture Behavioral of tb is
 
     Constant T: time := 20 ns;
-    Constant N : integer := 8;
-    signal clk, rst, establish: std_logic;
-    signal slave : std_logic_vector(1 downto 0);
-    signal data_in, data_out : std_logic_vector(N-1 downto 0);
-    signal data_width : unsigned(6 downto 0);
+    
+    signal clk : std_logic;
+    signal rst : std_logic;
+    --
+    signal start       : std_logic;
+    signal slave_width : std_logic;
+    signal slave       : std_logic_vector(1 downto 0);
+    --
+    signal data_in   : std_logic_vector(15 downto 0);
+    signal data_out  : std_logic_vector(15 downto 0);
     
 begin
     
-    yarb : entity work.SPI_UART(Behavioral)
-            port map(clk=>clk, rst=>rst, establish=>establish, data_width=>data_width,data_in=>data_in, data_out=>data_out, slave=>slave);
+    SPI_UART_Communication : entity work.SPI_UART(Behavioral)
+            port map(
+                clk => clk, 
+                rst => rst, 
+                --
+                start => start,
+                slave_width => slave_width,
+                slave => slave,
+                --
+                data_in => data_in, 
+                data_out => data_out);
 
 
     process
     begin
     
-    clk<='0';
+    clk <= '0';
     wait for T/2;
    
-    clk<='1';
+    clk <= '1';
     wait for T/2;
         
     end process;        
@@ -45,24 +60,35 @@ begin
     process
     begin
     
+    rst <= '1';
+    wait for 2*T;
+   
+    rst <= '0';
+    
+    start <= '1';
+    slave<="01";
+    
+    slave_width <= '1';
+    
+    data_in<="0000000011001100";
+    wait for 2ms;
+    
+    data_in<="0000000001010101";
+    wait for 2ms;
+    
     rst<='1';
     wait for 2*T;
    
     rst<='0';
-    wait for 3*T;
-    data_width<="0001000";
-    slave<="01";
-    data_in<="11001100";
-    establish<='1';
-    wait for 800us;
-    data_in<="01010101";
-    wait for 800us;
-    data_in<="11001100";
-    establish<='1';
-    wait for 800us;
-    data_in<="01010101";
+   
+    slave_width <= '0';
     
-    wait for 800us;
+    data_in<="1100010011001100";
+    wait for 2ms;
+    
+    data_in<="1101011010101010";
+    wait for 2ms;
+    
     end process;
     
 end Behavioral;
