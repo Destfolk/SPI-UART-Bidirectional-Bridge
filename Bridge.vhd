d86dad396754b7da268d9ab579fcbf96f447ca35
Destfolk <36056285+Destfolk@users.noreply.github.com>
2020-03-29 19:07:15 +0200

    Update Bridge.vhd

--- Bridge.vhd
+++ Bridge.vhd
@@ -35,7 +35,7 @@ end Bidirectional_Bridge;
 
 architecture Behavioral of Bidirectional_Bridge is
 
-type statetype is (ideal, start, MOSI, MISO, stop);
+    type statetype is (ideal, start, MOSI, MISO, stop);
 
     signal state     : statetype; 
     signal nextstate : statetype;
@@ -77,46 +77,46 @@ type statetype is (ideal, start, MOSI, MISO, stop);
 begin
     
      counter: entity work.Counter(Behavioral)
-               generic map (
-                   M => 162,
-                   N => 8)
-               port map (
-                   clk => clk,
-                   rst => rst,
-                   clk_out => clk_out);
+        generic map (
+            M => 162,
+            N => 8)
+        port map (
+            clk => clk,
+            rst => rst,
+            clk_out => clk_out);
             
             
             
      Btx: entity work.Tx(Behavioral)
-           generic map (
-               data_width => M,
-               stop_ticks => 16)
-           port map ( 
-               clk => clk,
-               rst => rst,
-               -- 
-               Tx_start => Tx_ready, 
-               --
-               qin => mem_reg1, 
-               Tx_out => mosi_out,
-               --
-               done => Tx_done);
+        generic map (
+            data_width => M,
+            stop_ticks => 16)
+        port map ( 
+            clk => clk,
+            rst => rst,
+            -- 
+            Tx_start => Tx_ready, 
+            --
+            qin => mem_reg1, 
+            Tx_out => mosi_out,
+            --
+            done => Tx_done);
      
      
      
      Brx: entity work.Rx(Behavioral)
-           generic map (
-               data_width => M, 
-               stop_ticks => 16)
-           port map ( 
-               clk => clk, 
-               rst => rst,
-               --
-               Rx_ready => Rx_ready,
-               --
-               Rx_in => miso_in, 
-               qout => mem_reg2, 
-               done => Rx_done);
+        generic map (
+            data_width => M, 
+            stop_ticks => 16)
+        port map ( 
+            clk => clk, 
+            rst => rst,
+            --
+            Rx_ready => Rx_ready,
+            --
+            Rx_in => miso_in, 
+            qout => mem_reg2, 
+            done => Rx_done);
                
                
          
@@ -124,136 +124,104 @@ begin
     begin
     
         if rising_edge(clk) then
-            
-            if(rst = '1') then
-            
+            if (rst = '1') then
                 state <= ideal;
-            
             else
-                
                 state <= nextstate;
                 Tx_ready <= Tx_ready_next;
                 Rx_ready <= Rx_ready_next;
                 n <= n_next;
-                
             end if;
-
-        end if;
-        
+        end if;        
     end process;
-    
     
     process(clk)
     begin
     
         case state is
-     
-        when ideal =>
-            
-            nextstate <= state;
-            n_next <= M;
+            when ideal =>
+                nextstate <= state;
+                n_next <= M;
                     
-            if(clk_out = '1') then
-                
-                if(establish(0) = '1') then
-                    n_next <= M/2;
-                    Tx_ready_next(0) <= '1';
-                    Rx_ready_next(0) <= '1';
-                    else
-                    Tx_ready_next(0) <= '0';
-                    Rx_ready_next(0) <= '0';
-                end if;
+                if (clk_out = '1') then
+                    if (establish(0) = '1') then
+                        n_next <= M/2;
+                        Tx_ready_next(0) <= '1';
+                        Rx_ready_next(0) <= '1';
+                        else
+                        Tx_ready_next(0) <= '0';
+                        Rx_ready_next(0) <= '0';
+                    end if;
                         
-                if(establish(1)= '1') then
-                    nextstate <= start;
+                    if (establish(1)= '1') then
+                        nextstate <= start;
+                    end if;
                 end if;
-                
-            end if;
             
-            
-        when start =>
-        
-            if(clk_out = '1') then
-            
-                case ss is
+            when start =>
+                if (clk_out = '1') then
+                    case ss is
+                        when  "0001" =>
+                            if (data_ready = '1') then
+                                nextstate <= MOSI; end if;
                 
-                    when  "0001" =>
-                        if (data_ready = '1') then
-                            nextstate <= MOSI; end if;
-                
-                    when  "0010" =>
-                        Rx_ready_next(1) <= '1';
-                        if(Rx_done = '1') then
-                            nextstate <= MISO; end if;
+                        when  "0010" =>
+                            Rx_ready_next(1) <= '1';
+                        
+                            if (Rx_done = '1') then
+                                nextstate <= MISO;
+                            end if;
                     
-                    when others =>
-                        Tx_ready_next(1) <= '0';
-                        Rx_ready_next(1) <= '0';         
-        
-                end case;
+                        when others =>
+                            Tx_ready_next(1) <= '0';
+                            Rx_ready_next(1) <= '0'; 
+                    end case;
                 
-                if(establish(1) = '0') then
-                    nextstate <= ideal;
+                    if (establish(1) = '0') then
+                        nextstate <= ideal;
+                    end if;
                 end if;
-                
-            end if;
             
-            
-        when MOSI =>
-            
-            if(clk_out = '1') then
-                
-                if(n > 0) then
-                    reg1(n-1) <= mosi_in;
-                    n_next <= n-1;                    
-                else
-                    
-                    if(mem_reg1/=reg1)then
-                        Tx_ready_next(1) <= '1';
-                        mem_reg1<=reg1;
+            when MOSI =>
+                if (clk_out = '1') then
+                    if (n > 0) then
+                        reg1(n-1) <= mosi_in;
+                        n_next <= n-1;                    
                     else 
+                        if (mem_reg1/=reg1) then
+                            Tx_ready_next(1) <= '1';
+                            mem_reg1<=reg1;
+                        else 
+                            nextstate <= stop;
+                        end if;
+                    end if;
+                
+                    if (Tx_done = '1') then
+                        Tx_ready_next(1) <= '0';
                         nextstate <= stop;
                     end if;
-                    
                 end if;
+            
+            when MISO =>
+                if (clk_out = '1') then
+                    reg2 <= '0' & mem_reg2;
                 
-                if(Tx_done = '1') then
-                    Tx_ready_next(1) <= '0';
-                    nextstate <= stop;
-                end if;
-                
-            end if;
-            
-            
-        when MISO =>
-            
-            if(clk_out = '1') then
-            
-                reg2 <= '0' & mem_reg2;
-                
-                if(n>=0) then
+                if (n>=0) then
                     miso_out <= reg2(n);
                     n_next <= n-1;
                 else
                     nextstate <= stop;
                     miso_out <= '1';
                 end if;
-              
             end if;
             
-        
-        when stop =>
-        
-            if(clk_out = '1') then
-            
+            when stop =>
+                if (clk_out = '1') then
                 Tx_ready_next(1) <= '0';
                 Rx_ready_next(1) <= '0';
                 nextstate <= ideal;
-                
             end if;  
-        
-        end case;        
-        
+        end case;   
     end process;
                     
 end Behavioral;
