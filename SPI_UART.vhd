15fdf2d2451be73fd46d6789510684c1b3bf12a2
Destfolk <36056285+Destfolk@users.noreply.github.com>
2020-03-29 12:10:34 +0200

    Update SPI_UART.vhd

--- SPI_UART.vhd
+++ SPI_UART.vhd
@@ -8,55 +8,184 @@
 --Email: destfolk@gmail.com
 ----------------------------------------------------------------------------
 
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.all;
 
 entity SPI_UART is
 
-    Port (clk        : in std_logic;
-          rst        : in std_logic;
-      
-          establish  : in std_logic;
-          slave      : in std_logic_vector(1 downto 0);
+    Port (clk         : in std_logic;
+          rst         : in std_logic;
           
-          data_width : in unsigned(6 downto 0);
+          start       : in std_logic;
+          slave_width : in std_logic;
+          slave       : in std_logic_vector(1 downto 0);
           
-          data_in    : in std_logic_vector(7 downto 0);
-          data_out: out std_logic_vector(7 downto 0)
+          data_in     : in std_logic_vector(15 downto 0);
+          data_out    : out std_logic_vector(15 downto 0)
     );
     
 end SPI_UART;
 
 architecture Behavioral of SPI_UART is
 
-signal ss                              :std_logic_vector(3 downto 0);
-signal mosi, miso,Rx_in, Tx_out        : std_logic;
-signal Rx_done, Tx_done                : std_logic;
-signal rx_ready, spi_ready, data_ready : std_logic;
-signal dout1, dout2                    : std_logic_vector(7 downto 0);
+    signal establish : std_logic_vector(1 downto 0);
+    
+    --------------------------
+    -- SPI Signals
+    --------------------------
+
+    signal data_ready : std_logic;
+    signal din1       : std_logic_vector(15 downto 0);
+    signal dout1      : std_logic_vector(15 downto 0);
+
+    --------------------------
+    -- Bridge Signals
+    --------------------------
+
+    signal mosi   : std_logic;
+    signal miso   : std_logic;
+    signal Rx_in  : std_logic;
+    signal Tx_out : std_logic;
+    signal ss     :std_logic_vector(3 downto 0);
+
+    --------------------------
+    -- Tx Signals
+    --------------------------
+
+    signal din2    : std_logic_vector(15 downto 0);
+    signal Tx_done : std_logic;
+
+    --------------------------
+    -- Rx Signals
+    --------------------------
+
+    signal dout2   : std_logic_vector(15 downto 0);
+    signal Rx_done : std_logic;
+
+
 
 begin
 
-    send: entity work.SPI_Master(Behavioral)
-          generic map (N=>8)
-          port map ( clk=>clk, rst=>rst, miso=>miso, mosi=>mosi,establish=>establish, data_ready=>data_ready,
-                     ss=>ss, slave=>slave, qin=>data_in,qout=>dout1);
-    
-    Bridge_test: entity work.Bidirectional_Bridge(Behavioral)
-        generic map (M=>8)
-        port map ( clk=>clk, rst=>rst, ss=>ss, establish=>establish,data_ready=>data_ready, mosi_in=>mosi, miso_in=>tx_out,
-                    miso_out=>miso,mosi_out=>rx_in);
 
-     txtrial: entity work.Tx(Behavioral)
-        generic map (data_width=>8, stop_ticks=>16)
-        port map ( clk=>clk, rst=>rst, Tx_start=>establish, qin=>data_in, Tx_out=>Tx_out, done=>Tx_done);
+    SPI: entity work.SPI_Master(Behavioral)
+          generic map (
+              N => 16)
+          port map (
+              clk => clk,
+              rst => rst, 
+              --
+              miso => miso,
+              mosi => mosi,
+              --
+              establish => establish,
+              data_ready => data_ready,
+              --
+              ss => ss, 
+              slave => slave,
+              -- 
+              qin => din1,
+              qout => dout1);
+    
+    
+    
+    Bridge: entity work.Bidirectional_Bridge(Behavioral)
+             generic map (
+                 M => 16)
+             port map ( 
+                 clk => clk,
+                 rst => rst,
+                 --
+                 ss => ss, 
+                 establish => establish,
+                 data_ready => data_ready,
+                 -- 
+                 mosi_in => mosi, 
+                 miso_in => tx_out,
+                 miso_out => miso,
+                 mosi_out => rx_in);
+
+
+
+    Tx: entity work.Tx(Behavioral)
+         generic map (
+             data_width => 16,
+             stop_ticks => 16)
+         port map ( 
+             clk => clk,
+             rst => rst,
+             -- 
+             Tx_start => establish,
+             -- 
+             qin => din2, 
+             Tx_out => Tx_out,
+             -- 
+             done => Tx_done);
                             
-                    
-    recieve: entity work.Rx(Behavioral)
-        generic map (data_width=>8, stop_ticks=>16)
-        port map ( clk=>clk, rst=>rst, Rx_ready=>establish, Rx_in=>Rx_in, qout=>dout2, done=>Rx_done);
-        
-    data_out <= dout1 when slave = "01" else dout2;
                 
+                    
+    Rx: entity work.Rx(Behavioral)
+         generic map (
+             data_width => 16,
+             stop_ticks => 16)
+         port map ( 
+             clk => clk, 
+             rst => rst, 
+             --
+             Rx_ready => establish, 
+             --
+             Rx_in => Rx_in, 
+             qout => dout2, 
+             --
+             done => Rx_done);
+             
+        
+    process(clk)
+    begin
+    
+        case start is
+            
+            when '1' =>
+                establish(1) <= '1';
+            
+            when others =>
+                establish(1) <= '0';
+                
+        end case;
+        
+        case slave_width is
+            
+            when '1' =>
+                establish(0) <= '1';    -- 8-bit
+            
+            when '0' =>
+                establish(0) <= '0';    -- 16-bit
+            
+            when others =>
+                null;
+                
+        end case;
+    
+        case slave is
+        
+            when "00" =>
+                din1 <= data_in;
+                data_out <= dout2;
+        
+            when "01" =>
+                din2 <= data_in;
+                data_out <= dout1;
+            
+            when others =>
+                null;
+                
+        end case;
+               
+    end process;   
+    
+    
+    --data_out <= dout1 when slave = "01" else dout2;
+    
+    
 end Behavioral;
