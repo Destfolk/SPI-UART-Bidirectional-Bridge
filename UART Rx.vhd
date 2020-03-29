3fbb80a3c48c51a17f3cdab9a82a994c2f5ea7d2
Destfolk <36056285+Destfolk@users.noreply.github.com>
2020-03-29 12:12:06 +0200

    Update UART Rx.vhd

--- UART Rx.vhd
+++ UART Rx.vhd
@@ -8,6 +8,7 @@
 --Email: destfolk@gmail.com
 ----------------------------------------------------------------------------
 
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -21,10 +22,10 @@ entity Rx is
     port( clk      : in std_logic;
           rst      : in std_logic;
     
-          Rx_ready : in std_logic;
+          Rx_ready : in std_logic_vector(1 downto 0);
           Rx_in    : in std_logic;
           
-          qout     : out std_logic_vector (7 downto 0);
+          qout     : out std_logic_vector (data_width-1 downto 0);
           done     : out std_logic
     );
           
@@ -32,20 +33,43 @@ end Rx;
 
 architecture Behavioral of Rx is
 
-type statetype is (ideal, start, data, stop);
+    type statetype is (ideal, start, data, stop);
 
-signal state, nextstate     : statetype;
-signal clk_out              : std_logic;
-signal num_reg, num_next    : unsigned (2 downto 0);
-signal sum_reg, sum_next    : unsigned (3 downto 0);
-signal data_reg, data_next  : std_logic_vector (7 downto 0);
-
-
+    signal state     : statetype; 
+    signal nextstate : statetype;
+    signal clk_out   : std_logic;
+    
+    --------------------------
+    -- Registers 
+    --------------------------
+    
+    signal data_reg  : std_logic_vector(data_width-1 downto 0);
+    signal data_next : std_logic_vector(data_width-1 downto 0);
+    
+        --------------------------
+    -- Counters 
+    --------------------------
+    
+    signal width    : integer;
+    --
+    signal sum_reg  : unsigned (3 downto 0);
+    signal sum_next : unsigned (3 downto 0);
+    --
+    signal num_reg  : unsigned (3 downto 0);
+    signal num_next : unsigned (3 downto 0);
+    
+    
+    
 begin
 
     counter: entity work.Counter(Behavioral)
-            generic map (M=>162, N=>8)
-            port map ( clk=>clk, rst=>rst, clk_out=>clk_out);
+              generic map (
+                  M => 162,
+                  N => 8)
+              port map ( 
+                  clk => clk, 
+                  rst => rst, 
+                  clk_out => clk_out);
         
     process(clk)
     begin
@@ -80,7 +104,8 @@ begin
             num_next <= num_reg;
             data_next <= data_reg;
             done <= '0';
-            if (Rx_in = '0' and Rx_ready = '1') then 
+            
+            if (Rx_in = '0' and Rx_ready(1) = '1') then 
                 nextstate <= start;
             end if;
         
@@ -88,7 +113,6 @@ begin
         when start =>  
               
             if (clk_out = '1') then 
-                
                 
                 if (sum_reg = 7) then  
                     nextstate <= data; 
@@ -103,13 +127,24 @@ begin
         when data =>
         
             if (clk_out = '1') then 
-            
+               
+               if(Rx_ready(0) = '1') then
+                    width <= data_width/2;
+               else 
+                    width <= data_width;
+               end if;
+                 
                if (sum_reg = 15) then 
                
                     sum_next <= (others => '0');
-                    data_next <= Rx_in & data_reg(7 downto 1);
                     
-                    if (num_reg = data_width -1 ) then 
+                    if(Rx_ready(0) = '1') then
+                        data_next <= "00000000"  & Rx_in & data_reg(data_width/2-1 downto 1);
+                    else 
+                        data_next <= Rx_in & data_reg(data_width-1 downto 1);
+                    end if;
+                    
+                    if (num_reg = width - 1 ) then 
                         nextstate <= stop;
                     else 
                         num_next <= num_reg + 1;
@@ -142,4 +177,5 @@ begin
      end process;
      
      qout <= data_reg;
+     
 end Behavioral;      
