bd2a9cf5fac8433357d3ef90d9370876b066f307
Destfolk <36056285+Destfolk@users.noreply.github.com>
2020-03-29 12:09:04 +0200

    Update SPI_Master.vhd

--- SPI_Master.vhd
+++ SPI_Master.vhd
@@ -1,12 +1,21 @@
 ----------------------------------------------------------------------------
+
 --UART-SPI BIDIRECTIONAL BRIDGE
+
 --SPI_Master.vhd
+
 --
+
 --GSoC 2020
+
 --
+
 --Copyright (C) 2020 Seif Eldeen Emad Abdalazeem
+
 --Email: destfolk@gmail.com
+
 ----------------------------------------------------------------------------
+
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -20,7 +29,7 @@ entity SPI_Master is
     Port ( clk        : in std_logic;
            rst        : in std_logic;
            
-           establish  : in  std_logic;
+           establish  : in  std_logic_vector(1 downto 0);
            slave      : in std_logic_vector(1 downto 0);
            
            qin        : in std_logic_vector(N-1 downto 0);
@@ -39,18 +48,52 @@ architecture Behavioral of SPI_Master is
 
     type statetype is (ideal, slave_select, connected, stop);
 
-    signal state, nextstate                   : statetype;
-    signal clk_out, mosi_next, miso_next      : std_logic;
-    signal Tx_ready, Tx_ready_next, reg_ready : std_logic;
-    signal i, i_next, j, j_next, x, x_next    : integer;
-    signal Tx_reg                             :  std_logic_vector(N-1 downto 0);
-   
-    
+    signal state     : statetype; 
+    signal nextstate : statetype;
+    signal clk_out   : std_logic;
+
+    --------------------------
+    -- MOSI Signals
+    --------------------------
+
+    signal mosi_next     : std_logic;
+    signal Tx_ready      : std_logic;
+    signal Tx_ready_next : std_logic;
+    signal Tx_reg        : std_logic_vector(N-1 downto 0);
+
+    --------------------------
+    -- MISO Signals
+    --------------------------    
+
+    signal miso_next : std_logic;
+
+    --------------------------
+    -- Counters 
+    --------------------------
+
+    signal i      : integer;
+    signal i_next : integer;
+
+    signal j      : integer;
+    signal j_next : integer;
+
+    signal x      : integer;
+    signal x_next : integer;
+
+    signal width  : integer;
+
+
+
 begin
     
     counter: entity work.Counter(Behavioral)
-            generic map (M=>162, N=>8)
-            port map ( clk=>clk, rst=>rst, clk_out=>clk_out);
+              generic map (
+                  M => 162,
+                  N => 8)
+              port map ( 
+                  clk => clk,
+                  rst => rst, 
+                  clk_out => clk_out);
 
     
     process (clk)
@@ -95,10 +138,17 @@ begin
                     j_next <= j;
                     x_next <= 0;
                     Tx_ready_next <= '0';
+                    width<=N;
                     
                     if(clk_out <= '1') then
                         
-                        if(establish = '1') then
+                        if(establish(0) = '1') then
+                            i_next <= N/2;
+                            j_next <= N/2;
+                            width<= N/2;
+                        end if;
+                        
+                        if(establish(1) = '1') then
                             nextstate <= slave_select;
                         end if;
                     
@@ -139,7 +189,7 @@ begin
                     
                     if(clk_out = '1') then
                         
-                        if(establish = '0') then
+                        if(establish(1) = '0') then
                             nextstate <= stop;
                         else
                         
@@ -149,18 +199,21 @@ begin
                                 
                                 if(Tx_ready <= '1') then 
                                     
-                                    if (i = N) then
+                                    if (i = width) then
                                         Tx_reg <= qin;
                                         data_ready <= '1';
                                     end if;
                                     
-                                    if(i=N-1) then data_ready <= '0';end if;
+                                    if(i = width - 1) then
+                                        data_ready <= '0';
+                                    end if;
                                     
                                     if(i = 0) then
                                         i_next <= i-1;
-                                        elsif(i=-1) then nextstate <= stop;
+                                    elsif(i=-1) then 
+                                        nextstate <= stop;
                                         data_ready <= '0';
-                                     else
+                                    else
                                         mosi_next <= Tx_reg(i-1);
                                         i_next <= i-1;
                                     end if;
@@ -176,8 +229,12 @@ begin
                                 
                                 if(x = 1) then
                                 
+                                    if(establish(0)='1') then 
+                                        qout(15 downto 8) <= "00000000"; 
+                                    end if;
+                                    
                                     if(j = 0) then
-                                        j_next <= N;
+                                        j_next <= width;
                                         nextstate <= stop;
                                     else 
                                         qout(j-1) <= miso_next;
@@ -201,8 +258,7 @@ begin
                         j_next <= N;
                         x_next <= 0;
                         nextstate <= ideal;
-                        
-                        
+                      
                     end if;
                             
         end case;
