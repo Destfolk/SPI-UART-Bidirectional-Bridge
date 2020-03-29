ff5fdeb4f1c750073e7eafa97e2d167511405a0f
Destfolk <36056285+Destfolk@users.noreply.github.com>
2020-03-29 12:16:44 +0200

    Update Bridge.vhd

--- Bridge.vhd
+++ Bridge.vhd
@@ -1,19 +1,11 @@
 ----------------------------------------------------------------------------
-
 --UART-SPI BIDIRECTIONAL BRIDGE
-
 --Bridge.vhd
-
 --
-
 --GSoC 2020
-
 --
-
 --Copyright (C) 2020 Seif Eldeen Emad Abdalazeem
-
 --Email: destfolk@gmail.com
-
 ----------------------------------------------------------------------------
 
 
